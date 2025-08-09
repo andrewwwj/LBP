ec114b4ef7be12d820e49c9c0b179b2e229b69af
andrewwwj <andreww7447@gmail.com>
2025-08-10 00:16:45 +0900

    .

--- models/MidPlanner.py
+++ models/MidPlanner.py
@@ -5,6 +5,7 @@ import DecisionNCE
 import random
 import pickle
 
+
 def init_weight(m):
     if isinstance(m, nn.Linear):
         nn.init.kaiming_normal_(m.weight)
@@ -14,12 +15,14 @@ def init_weight(m):
         nn.init.constant_(m.bias, 0)
         nn.init.constant_(m.weight, 1.0)
 
+
 class DualPathPredictor(nn.Module):
-    def __init__(self, 
-                 latent_dim = 1024, 
-                 output_dim = 1024):
+    def __init__(self,
+                 latent_dim=1024,
+                 output_dim=1024):
         super().__init__()
-        self.predictor = Mlp(in_features=latent_dim * 2, hidden_features = latent_dim*4, out_features = output_dim, norm_layer=nn.LayerNorm)
+        self.predictor = Mlp(in_features=latent_dim * 2, hidden_features=latent_dim * 4, out_features=output_dim,
+                             norm_layer=nn.LayerNorm)
         self.apply(init_weight)
 
     def forward(self, s0, s1):
@@ -27,17 +30,18 @@ class DualPathPredictor(nn.Module):
         pred = self.predictor(s)
         return pred
 
+
 class TriplePathPredictor(nn.Module):
-    def __init__(self, 
-                 latent_dim = 1024, 
-                 output_dim = 1024):
+    def __init__(self,
+                 latent_dim=1024,
+                 output_dim=1024):
         super().__init__()
         self.predictor = Mlp(in_features=latent_dim * 3,
-                             hidden_features = latent_dim*4,
-                             out_features = output_dim,
+                             hidden_features=latent_dim * 4,
+                             out_features=output_dim,
                              norm_layer=nn.LayerNorm)
         self.apply(init_weight)
-        
+
     def forward(self, s0, s1, s2):
         s = torch.cat([s0, s1, s2], dim=-1)
         pred = self.predictor(s)
@@ -88,18 +92,18 @@ class DnceLatentProj(nn.Module):
                 data = pickle.load(f)
             img_mean = torch.tensor(data['img_latents_dnce']['image0']['mean'])
             img_std = torch.tensor(data['img_latents_dnce']['image0']['std'])
-        else :
+        else:
             img_mean = torch.zeros(1024)
             img_std = torch.ones(1024)
 
         self.register_buffer('img_mean', img_mean)
         self.register_buffer('img_std', img_std)
-    
+
     def img_proj(self, x):
         x = self.latent_proj.model.encode_image(x)
         x = (x - self.img_mean) / self.img_std
         return x
-    
+
     def lang_proj(self, x):
         x = self.latent_proj.encode_text(x)
         return x
@@ -107,14 +111,14 @@ class DnceLatentProj(nn.Module):
 
 class MidImaginator(nn.Module):
     def __init__(
-        self, 
-        latent_dim = 1024, 
-        recursive_step = 4,
-        state_random_noise = True,
-        state_noise_strength = 0.1,
-        loss_func = nn.MSELoss,
-        loss_func_conig = dict(reduction='mean'),
-        latent_info_file='assets/libero.pkl'
+            self,
+            latent_dim=1024,
+            recursive_step=4,
+            state_random_noise=True,
+            state_noise_strength=0.1,
+            loss_func=nn.MSELoss,
+            loss_func_conig=dict(reduction='mean'),
+            latent_info_file='assets/libero.pkl'
     ):
         super().__init__()
         self.latent_dim = latent_dim
@@ -131,10 +135,10 @@ class MidImaginator(nn.Module):
         sg = self.latent_proj.lang_proj(instruction)
         s0 = self.latent_proj.img_proj(cur_images[:, 0, ...])
         B, G, C, H, W = sub_goals.shape  # subgoals: a series of images during training
-        sub_goals = sub_goals.reshape(B*G, C, H, W)
+        sub_goals = sub_goals.reshape(B * G, C, H, W)
         sub_goals = self.latent_proj.img_proj(sub_goals)
         sub_goals = sub_goals.reshape(B, G, -1)
-        
+
         loss_dict = {}
         # Predict previous latents of the last sub-goal
         pred_subgoal = self.goal_rec(s0, sg)
@@ -143,29 +147,26 @@ class MidImaginator(nn.Module):
 
         # Recursive sub-goal prediction
         # random_number = random.random()
-        random_number = torch.rand(1, device=sub_goals.device) < 0.5
-        if random_number < 0.5:
-            for i in range(1, self.recursive_step):
+        random_number = torch.rand(1, device=sub_goals.device)
+        for i in range(1, self.recursive_step):
+            target_subgoal = sub_goals[:, i, ...]
+            if random_number < 0.5:
                 # latent planners
                 last_subgoal = pred_subgoal
-                target_subgoal = sub_goals[:, i, ...]
                 pred_subgoal = self.latent_planner(s0, last_subgoal, sg)
-                loss_dict[f"loss_latent_w{i}"] = self.loss_func(pred_subgoal, target_subgoal)
-        else :
-            for i in range(1, self.recursive_step):
+            else:
                 # latent planners
-                last_subgoal = sub_goals[:, i-1, ...]
-                target_subgoal = sub_goals[:, i, ...]
+                last_subgoal = sub_goals[:, i - 1, ...]
                 if self.state_random_noise:
                     random_noise = torch.randn_like(last_subgoal) * self.state_noise_strength
                     last_subgoal = last_subgoal + random_noise
                 pred_subgoal = self.latent_planner(s0, last_subgoal, sg)
-                loss_dict[f"loss_latent_w{i}"] = self.loss_func(pred_subgoal, target_subgoal)
+            loss_dict[f"loss_latent_w{i}"] = self.loss_func(pred_subgoal, target_subgoal)
 
         loss = sum(loss_dict.values()) / len(loss_dict)
         loss_dict['loss'] = loss
         return loss, loss_dict
-    
+
     def generate(self, cur_images, instruction, recursive_step, **kwargs):
         sg = self.latent_proj.lang_proj(instruction)
         s0 = self.latent_proj.img_proj(cur_images[:, 0, ...])
@@ -180,9 +181,9 @@ class MidImaginator(nn.Module):
 
 
 def mid_planner_dnce_noise(recursive_step=4, **kwargs):
-    return MidImaginator(recursive_step = recursive_step,
-                         state_random_noise = True,
-                         state_noise_strength = 0.1,
-                         loss_func = nn.MSELoss,
-                         loss_func_conig = dict(reduction='mean'),
+    return MidImaginator(recursive_step=recursive_step,
+                         state_random_noise=True,
+                         state_noise_strength=0.1,
+                         loss_func=nn.MSELoss,
+                         loss_func_conig=dict(reduction='mean'),
                          latent_info_file='assets/libero.pkl')
